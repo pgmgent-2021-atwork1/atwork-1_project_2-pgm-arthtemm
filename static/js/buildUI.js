beb6eba961e48db1c832b06755ee3a52e3554d59
pgm-arthtemm <arthur.temmerman@student.arteveldehs.be>
2020-12-18 19:03:49 +0100

    commit

---
+++ static/js/buildUI.js
@@ -0,0 +1,47 @@
+(() => {
+    const app = {
+        initialize: function () {
+            this.cacheElements();
+            this.RegisterListeners();
+        },
+        cacheElements: function () {
+            this.$eventList = document.querySelector('.event-list');
+            this.$hamburgerMenu = document.querySelector('.hamburger-menu');
+            this.$closeButton = document.querySelector('.close-btn');
+            this.$navigation = document.querySelector('.navigation');
+            this.$menuButton = document.querySelector('.menu');
+            this.$programDropdown = document.querySelector('.icon_dropdown_program');
+            this.$programDropdownList = document.querySelector('.program_dropdown');
+            this.$program = document.querySelector('.program');
+            this.$dayEventList = document.querySelector('.day-event-list');
+        },
+
+        RegisterListeners: function () {
+            this.$menuButton.addEventListener('click', () => {
+                this.$hamburgerMenu.classList.add('open');
+                this.$closeButton.classList.add('open');
+                this.$menuButton.classList.add('open');
+                this.$navigation.classList.add('open');
+            });
+            this.$closeButton.addEventListener('click', () => {
+                this.$hamburgerMenu.classList.remove('open');
+                this.$closeButton.classList.remove('open');
+                this.$menuButton.classList.remove('open');
+                this.$navigation.classList.remove('open');
+            });
+            let dropDownOpen = false;
+            this.$programDropdown && this.$program.addEventListener('click', () => {
+                if (!dropDownOpen) {
+                    this.$programDropdownList.classList.add('open');
+                    this.$programDropdown.classList.add('open');
+                    dropDownOpen = true;
+                } else {
+                    this.$programDropdownList.classList.remove('open');
+                    this.$programDropdown.classList.remove('open');
+                    dropDownOpen = false;
+                }
+            });
+        },
+    };
+    app.initialize();
+})();
--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,7 @@
-const EVENTS_API = 'https://www.pgm.gent/data/gentsefeesten/events.json';
+const EVENTS_API = 'https://www.pgm.gent/data/gentsefeesten/events_500.json';
+const CATEGORY_API = 'https://www.pgm.gent/data/gentsefeesten/categories.json';
 
-const EVENT_PICTURE_ARRAY = ['https://data.stad.gent/explore/dataset/gentse-feesten-evenementen-2019/files/26e0734c1a56705115083cb555ba57da/300',
-    "https://data.stad.gent/explore/dataset/gentse-feesten-evenementen-2019/files/86ea9e7de4645e39a34399d8e6ae1f31/300",
-    "https://data.stad.gent/explore/dataset/gentse-feesten-evenementen-2019/files/85456f6e6c9913789b740d9c21eca3d1/300"
-];
+const FALLBACK_IMAGE = 'https://data.stad.gent/explore/dataset/gentse-feesten-evenementen-2019/files/26e0734c1a56705115083cb555ba57da/300';
 
 
 (() => {
@@ -11,7 +9,7 @@ const EVENT_PICTURE_ARRAY = ['https://data.stad.gent/explore/dataset/gentse-fees
         initialize: function () {
             this.cacheElements();
             this.getDataFromEventsAPIEndpoint();
-            this.RegisterListeners();
+            this.getDataFromCategoriesAPI();
         },
         cacheElements: function () {
             this.$eventList = document.querySelector('.event-list');
@@ -23,6 +21,8 @@ const EVENT_PICTURE_ARRAY = ['https://data.stad.gent/explore/dataset/gentse-fees
             this.$programDropdownList = document.querySelector('.program_dropdown');
             this.$program = document.querySelector('.program');
             this.$dayEventList = document.querySelector('.day-event-list');
+            this.$dayFilterCategories = document.querySelector('.day_filter_categories');
+            this.$eventsPerCategory = document.querySelector('.events_per_category');
         },
         getDataFromEventsAPIEndpoint() {
             fetch(EVENTS_API, {})
@@ -45,32 +45,80 @@ const EVENT_PICTURE_ARRAY = ['https://data.stad.gent/explore/dataset/gentse-fees
                 .then(json => this.updateDayEvents(json))
                 .catch(error => console.warn(error));
         },
-        RegisterListeners: function () {
-            this.$menuButton.addEventListener('click', () => {
-                this.$hamburgerMenu.classList.add('open');
-                this.$closeButton.classList.add('open');
-                this.$menuButton.classList.add('open');
-                this.$navigation.classList.add('open');
-            });
-            this.$closeButton.addEventListener('click', () => {
-                this.$hamburgerMenu.classList.remove('open');
-                this.$closeButton.classList.remove('open');
-                this.$menuButton.classList.remove('open');
-                this.$navigation.classList.remove('open');
-            });
-            let dropDownOpen = false;
-            this.$programDropdown && this.$program.addEventListener('click', () => {
-                if (!dropDownOpen) {
-                    this.$programDropdownList.classList.add('open');
-                    this.$programDropdown.classList.add('open');
-                    dropDownOpen = true;
-                } else {
-                    this.$programDropdownList.classList.remove('open');
-                    this.$programDropdown.classList.remove('open');
-                    dropDownOpen = false;
-                }
-            });
+
+        getDataFromCategoriesAPI() {
+            fetch(CATEGORY_API)
+                .then((response) => response.json())
+                .then((json) => {
+                    console.log(json);
+                    this.categories = json;
+                    this.fetchEvents();
+                })
+                .catch((error) => console.log(error));
         },
+
+        fetchEvents() {
+            fetch(EVENTS_API)
+                .then((response) => response.json())
+                .then((json) => {
+                    this.events = json;
+                    console.log(json);
+                    this.populateHTMLForCategoryList();
+                    this.populateHTMLForFullOverview();
+                })
+                .catch((error) => console.log(error));
+        },
+
+        populateHTMLForCategoryList() {
+            const htmlForCategories = this.categories.map((category) => {
+                return `
+                    <section>
+                        <a href="#${category}">
+                        <h2>${category}</h2>
+                        </a>
+                    </section>
+                `;
+            }).join('');
+            this.$dayFilterCategories.innerHTML = htmlForCategories;
+        },
+
+        populateHTMLForFullOverview() {
+            const htmlForFullOverview = this.categories.map((category) => {
+                const filteredEvents = this.events.filter((event) => {
+                    return event.category.indexOf(category) > -1;
+                });
+
+                const listItems = filteredEvents.map((event) => {
+                    return `
+                    <li class="highlight__item">
+                    <a class="highlight-item__link" href="#">
+                        <div class="link__top">
+                            <div class="highlight__img">
+                                <img class="highlight__img__img" src="${event.image == null ? FALLBACK_IMAGE : event.image.thumb}" alt="${event.title}">
+                            </div>
+                        </div>
+                        <div class="highlight-bottom__content">
+                            <span class="bottom__date"><span class="date__day">${event.day_of_week.slice(0,2)} ${event.day} Jul</span> <span class="date__time">${event.start} u.</span></span>
+                            <h2 class="bottom__title">${event.title}</h2>
+                            <span class="bottom__loc">${event.location}</span>
+                        </div>
+                    </a>
+                </li>`;
+                }).join('');
+
+                return `
+                    <section>
+                        <h2>${category}</h2>
+                        <ul>
+                            ${listItems}
+                        </ul>
+                    </section>
+                `;
+            }).join('');
+
+            this.$eventsPerCategory.innerHTML = htmlForFullOverview;
+        },
+
         updateEventsUI(data) {
             let tempStr = '';
             for (let i = 0; i < 3; i++) {
@@ -80,7 +128,7 @@ const EVENT_PICTURE_ARRAY = ['https://data.stad.gent/explore/dataset/gentse-fees
                 <a class="highlight-item__link" href="#">
                     <div class="link__top">
                         <div class="highlight__img">
-                            <img class="highlight__img__img" src="${randomNummer.image == null ? EVENT_PICTURE_ARRAY[Math.floor(Math.random() * EVENT_PICTURE_ARRAY.length)] : randomNummer.image.thumb}" alt="${randomNummer.title}">
+                            <img class="highlight__img__img" src="${randomNummer.image == null ? FALLBACK_IMAGE : randomNummer.image.thumb}" alt="${randomNummer.title}">
                         </div>
                     </div>
                     <div class="highlight-bottom__content">
